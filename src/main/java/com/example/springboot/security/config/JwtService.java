1becaf5d061c0f28cb0111220f6f0a4eb5e8c4fe
Held Grijo <held.grijo@gmail.com>
2023-07-16 11:05:26 -0400

    feat: validate token

--- src/main/java/com/example/springboot/security/config/JwtService.java
+++ src/main/java/com/example/springboot/security/config/JwtService.java
@@ -40,6 +40,19 @@ public class JwtService {
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
     }
